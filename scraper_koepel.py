babdd0a5c2c44162b0acb9b1770e4d0265882c75
dries-vereecken <dries.vereecken@pythonpredictions.com>
2025-05-09 22:52:48 +0200

    Update scrapers to use system ChromeDriver with headless mode

--- scraper_koepel.py
+++ scraper_koepel.py
@@ -1,1 +1,10 @@
- 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=chrome_options) 
